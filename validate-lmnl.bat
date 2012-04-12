f4507a1b58e9c4553162dc5e3cffffff35e4c37d
Wendell Piez <wendellpiez@gmail.com>
2012-04-12 19:07:22 -0400

    removed unneeded batch files

--- validate-lmnl.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem @echo Validating %1 as well-formed LMNL syntax ...
-call C:\Users\Wendell\Projects\LMNL\2012-demo\lmnl-validate-xpl lmnl-file=%1 > valid.xml
-xsltproc C:\Users\Wendell\Projects\LMNL\2012-demo\bin\support\report-plaintext.xsl valid.xml
-rem del valid.xml
