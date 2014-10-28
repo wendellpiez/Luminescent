102e47051d8b9b9adc25c02df2f8b762b3616902
Wendell Piez <wapiez@wendellpiez.com>
2014-10-28 16:50:27 -0400

    Removing Calabash test (success)

--- calabash-test/test-calabash.bat
+++
@@ -1,8 +0,0 @@
-set CALABASH_LMNL=C:\Bin\xmlcalabash-1.0.21-95\calabash LMNL-xLMNL.xpl
-
-rem LMNL file should be a URI, absolute or relative to the lib/up subdirectory
-
-set LMNLFILE=../../advicetoacaterpillar.lmnl
-
-echo Invoking Calabash with string option provided
-%CALABASH_LMNL% lmnl-file=%LMNLFILE%
