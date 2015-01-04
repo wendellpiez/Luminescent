d95b21d7578d66044ad2f4c6104f98cbf2b71ee3
Wendell Piez <wapiez@wendellpiez.com>
2015-01-04 13:14:24 -0500

    Tweaks to scripts and notes (syntax checking under Linux)

--- LMNLwf-calabash.sh
+++ LMNLwf-calabash.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-FILE=$1
+FILENAME=$1
 CALABASH=/home/wendell/xmlcalabash-1.0.23-96/calabash.jar
-PIPE=file:///home/wendell/Documents/Luminescent/LMNL-wf-check.xpl
+PIPELINE=file:///home/wendell/Documents/Luminescent/LMNL-wf-check.xpl
 
-echo LMNL syntax checking ... $FILE ...
-java -Xmx1024m -jar $CALABASH -dtext/plain@$FILE $PIPE
+echo LMNL syntax checking ... $FILENAME ...
+java -Xmx1024m -jar $CALABASH -dtext/plain@$FILE $PIPELINE
